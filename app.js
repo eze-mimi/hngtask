7c26c90644b7e221cd07a1cb9e1e7419586ec054
Miracle Eze <83085060+eze-mimi@users.noreply.github.com>
2022-10-28 11:21:24 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const fs = require('fs')
+require('dotenv').config()
+const cors = require('cors')
 
 const app = express()
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use(cors({
+    origin: '*'
+}));
 
 app.get('/', (req,res) => {
 
@@ -13,6 +19,8 @@ app.get('/', (req,res) => {
     res.send(userjson)
 })
 
-app.listen(3000, ()=>{
-    console.log('listening to port 3000')
+const PORT = process.env.PORT
+
+app.listen(PORT, ()=>{
+    console.log(`listening to port ${PORT}`)
 })
